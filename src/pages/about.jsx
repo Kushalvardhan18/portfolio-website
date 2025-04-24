9e46df90461ab3fed095606fb22a84ba13565267
Kushal Vardhan <kushalvardhan1804@hotmail.com>
2025-04-24 22:18:47 +0530

    about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 function About() {
-  return <></>;
+  return <>
+  <div>
+  <h2>SOFTWARE DEVELOPER</h2>
+  <p></p>
+  <img src="" alt="" />
+  </div>
+  <div>
+    <img src="" alt="" />
+  </div>
+  </>;
 }
 export default About;
